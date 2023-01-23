05b79f489b36e48204fbe778af9e051198c22734
Aysel Dilan Bulut <ayseldilanbulut@Aysels-MacBook-Pro.local>
2023-01-23 10:18:53 +0000

    working on navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 //import Navbar from "@/scenes/navbar";
 import Navbar from "./scenes/navbar";
+import { useState } from "react";
 
-function App(): JSX.Element {
+enum SelectedPage {
+  Home = "home",
+  Benefits = "benefits",
+  OurClasses = "ourclasses",
+  ContactUs = "contactus"
+}
+
+function App(){
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
     <div className="app bg-gray-20">
-      <Navbar />
+      <Navbar 
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage} isTopOfPage={false}/>
     </div>
-  )
-}
+  );
+};
 
 export default App;
