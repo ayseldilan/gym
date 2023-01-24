69a89805d4f6fd05e3c64ae2c1865fbd9fd13381
Aysel Dilan Bulut <ayseldilanbulut@Aysels-MacBook-Pro.local>
2023-01-24 11:02:23 +0000

    finished navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-//import Navbar from "@/scenes/navbar";
-import Navbar from "./scenes/navbar";
-import { useState } from "react";
+import Navbar from "@/scenes/navbar";
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
-enum SelectedPage {
-  Home = "home",
-  Benefits = "benefits",
-  OurClasses = "ourclasses",
-  ContactUs = "contactus"
-}
-
-function App(){
+function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
-      <Navbar 
+      <Navbar
+        isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
-        setSelectedPage={setSelectedPage} isTopOfPage={false}/>
+        setSelectedPage={setSelectedPage}
+      />
+      
     </div>
   );
-};
+}
 
 export default App;
